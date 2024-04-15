7e672c6dff678ead3ef44d5f0cadd5f207b1dab2
Ravindra_Reddy <reddyravindrat4512@gmail.com>
2024-04-15 21:08:08 +0530

    test precommit hook

---
+++ test.js
@@ -0,0 +1,7 @@
+// test.js
+
+// This line intentionally violates the "no-console" rule
+console.log("Testing ESLint with Google style guide...");
+
+// This line intentionally violates the "no-unused-vars" rule (if not disabled)
+const unusedVariable = "This variable is not used";
